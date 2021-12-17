775c4bf009d92dd1e3c986c857578243dad45bcb
YKS <styleyo12@naver.com>
2021-12-17 14:56:36 +0900

    no message

---
+++ src/main/java/Project_5/AllRecommend/AllRecommendApplication.java
@@ -0,0 +1,13 @@
+package Project_5.AllRecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllRecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllRecommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project_5/AllRecommend/AllRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package Project_5.AllRecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
